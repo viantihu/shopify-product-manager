a503138d5a4a4af2859e4bc4bd6b274eb1741b6e
Karvi Bhatnagar <karvibhatnagar@VibhusMacStudio.frontiernet.net>
2026-06-20 13:51:34 -0500

    feat: recipe registry constants

---
+++ app/recipes/registry.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import { RECIPES, recipeRef } from "./registry";
+
+describe("recipe registry", () => {
+  it("exposes the four completeness recipes with versions", () => {
+    expect(Object.keys(RECIPES).sort()).toEqual([
+      "description-formatter",
+      "image-alt-text",
+      "product-type-inferrer",
+      "seo-meta-generator",
+    ]);
+  });
+
+  it("formats a recipe ref as id@version", () => {
+    expect(recipeRef("description-formatter")).toBe("description-formatter@1");
+  });
+});
